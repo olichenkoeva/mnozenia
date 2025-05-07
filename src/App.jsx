58c1c4c2c00375205ad3e4a215e6e5b97c8267fc
olichenkoeva <olicenkoeva2@gmail.cpm>
2025-05-07 20:38:16 +0200

    mnozenia1

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-
 import './App.css'
 
 function App() {
@@ -6,9 +5,62 @@ function App() {
 
   return (
     <>
-      i love stas
-    </>
+
+
+
+
+() {
+   (
+    <div className="app">
+      
+      <div className="header-frame">
+        <img
+          src="https://c.wallhere.com/photos/00/ad/flower_japan_garden_spring_nikon-408087.jpg!d"
+          alt="Header Background"
+          className="header-background"
+        />
+        <h1>Komputer numer - 4</h1>
+      </div>
+
+      
+      <div className="frame image-frame">
+        <div className="multiplication-table">
+          <p>4×1=4</p>
+          <p>4×2=8</p>
+          <p>4×3=12</p>
+          <p>4×4=16</p>
+          <p>4×5=20</p>
+          <p>4×6=24</p>
+          <p>4×7=28</p>
+          <p>4×8=32</p>
+          <p>4×9=36</p>
+          <p>4×10=40</p>
+        </div>
+      </div>
+
+      
+      <div className="frame transparent-frame">
+        <ul className="equations">
+          <li>4x³ + 1 - 4y = 36</li>
+          <li>2x + 4² - 4y² = 17</li>
+          <li>3y² - 4x²y - 4y = -78</li>
+          <li>2xy + 4xy = 54</li>
+          <li>3x² + 4y + 2 = 35</li>
+        </ul>
+      </div>
+    </div>
   )
 }
+
+
+
+    </>
+
+  )
+}
+    
+
+
+
 
 export default App
